11e199588b94d0167c9f02e5d4d96a8f2f968cea
aliwael <dev.aliwaell@gmail.com>
2025-08-07 10:14:56 +0300

    تحديث ServiceProvider لنشر الميجريشن وملف الهيلبر تلقائياً

--- src/Providers/PermissionsServiceProvider.php
+++ src/Providers/PermissionsServiceProvider.php
@@ -13,6 +13,12 @@ class PermissionsServiceProvider extends ServiceProvider
         $router = $this->app['router'];
         $router->aliasMiddleware('permission', \Permissions\Middleware\CheckPermission::class);
         $router->aliasMiddleware('role', \Permissions\Middleware\CheckRole::class);
+
+        // Publish migrations automatically
+        $this->publishes([
+            __DIR__ . '/../../database/migrations' => database_path('migrations'),
+            __DIR__ . '/../../src/helpers.php' => app_path('helpers/permissions_helpers.php'),
+        ], 'permissions-assets');
     }
 
     public function register()
